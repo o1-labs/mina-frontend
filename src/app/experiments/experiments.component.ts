8e5326e02c9ec82a99c7ea6d8dc210ee28298f0d
Serhii Shymkiv <sergey@shimkiv.com>
2025-01-14 17:49:50 +0200

    Experiments tab (styled).

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -8,6 +8,21 @@ import { ExperimentsService } from '../services/experiments.service';
 })
 export class ExperimentsComponent implements OnInit {
   experiments: any[] = [];
+  displayedColumns: string[] = [
+    'exp',
+    'round',
+    'rate_min',
+    'payment_rate_min',
+    'zkapp_rate_min',
+    'duration',
+    'max_latency',
+    'missed',
+    'failed',
+    'successful',
+    'start',
+    'total',
+    'zkapps',
+  ];
   isLoading = false;
 
   constructor(private readonly experimentsService: ExperimentsService) {}
@@ -28,5 +43,29 @@ export class ExperimentsComponent implements OnInit {
         this.isLoading = false;
       },
     });
+  }
+
+  refreshData(): void {
+    this.fetchExperiments();
+  }
+
+  shouldShowExperiment(index: number): boolean {
+    if (index === 0) {
+      return true;
+    }
+    return this.experiments[index].exp !== this.experiments[index - 1].exp;
+  }
+
+  getRowSpan(index: number): number {
+    const currentExp = this.experiments[index].exp;
+    let count = 0;
+    for (let i = index; i < this.experiments.length; i++) {
+      if (this.experiments[i].exp === currentExp) {
+        count++;
+      } else {
+        break;
+      }
+    }
+    return count;
   }
 }
--- src/app/experiments/experiments.module.ts
+++ src/app/experiments/experiments.module.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
+import { MatTableModule } from '@angular/material/table';
 import { ExperimentsRoutingModule } from './experiments-routing.module';
 import { ExperimentsComponent } from './experiments.component';
 
 @NgModule({
   declarations: [ExperimentsComponent],
-  imports: [CommonModule, ExperimentsRoutingModule, MatProgressSpinnerModule],
+  imports: [
+    CommonModule,
+    ExperimentsRoutingModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+  ],
 })
 export class ExperimentsModule {}
