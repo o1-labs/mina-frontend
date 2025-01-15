d2ffd1e9496d5fe65528152766372f64e220e0f8
Serhii Shymkiv <sergey@shimkiv.com>
2025-01-15 10:46:42 +0200

    Experiments tab (common).

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -1,41 +1,105 @@
 import { Component, OnInit } from '@angular/core';
+import { MinaTableWrapper } from '@app/shared/base-classes/mina-table-wrapper.class';
+import { TableColumnList } from '@app/shared/types/shared/table-head-sorting.type';
+import {
+  SortDirection,
+  TableSort,
+} from '@app/shared/types/shared/table-sort.type';
 import { ExperimentsService } from '../services/experiments.service';
+
+interface Experiment {
+  exp: string;
+  round: number;
+  rate_min: string;
+  zkapp_rate_min: string;
+  payment_rate_min: string;
+  duration: {
+    days?: number;
+    hours?: number;
+    minutes?: number;
+    seconds?: number;
+    milliseconds?: number;
+  };
+  max_latency: {
+    days?: number;
+    hours?: number;
+    minutes?: number;
+    seconds?: number;
+    milliseconds?: number;
+  };
+  missed: string;
+  failed: string;
+  successful: string;
+  start: string;
+  total: string;
+  zkapps: string;
+}
 
 @Component({
   selector: 'mina-experiments',
   templateUrl: './experiments.component.html',
   styleUrls: ['./experiments.component.scss'],
 })
-export class ExperimentsComponent implements OnInit {
-  experiments: any[] = [];
-  displayedColumns: string[] = [
-    'exp',
-    'round',
-    'rate_min',
-    'payment_rate_min',
-    'zkapp_rate_min',
-    'duration',
-    'max_latency',
-    'missed',
-    'failed',
-    'successful',
-    'start',
-    'total',
-    'zkapps',
+export class ExperimentsComponent
+  extends MinaTableWrapper<Experiment>
+  implements OnInit
+{
+  experiments: Experiment[] = [];
+  protected readonly tableHeads: TableColumnList<Experiment> = [
+    { name: 'experiment' },
+    { name: 'round' },
+    { name: 'total rate/min' },
+    { name: 'value transfer rate/min' },
+    { name: 'zkApp rate/min' },
+    { name: 'duration' },
+    { name: 'max latency' },
+    { name: 'missed rate' },
+    { name: 'failed rate' },
+    { name: 'successful rate' },
+    { name: 'start timestamp' },
+    { name: 'total txns' },
+    { name: 'zkApp txns' },
   ];
   isLoading = false;
 
-  constructor(private readonly experimentsService: ExperimentsService) {}
+  constructor(private readonly experimentsService: ExperimentsService) {
+    super();
+  }
 
-  ngOnInit(): void {
+  override async ngOnInit(): Promise<void> {
+    await super.ngOnInit();
     this.fetchExperiments();
+  }
+
+  protected override setupTable(): void {
+    this.table.gridTemplateColumns = [
+      250, 150, 150, 200, 150, 150, 150, 150, 150, 150, 300, 150, 150,
+    ];
+    this.table.minWidth = 998;
+    this.table.sortClz = class {
+      constructor(public payload: TableSort<Experiment>) {
+        this.type = 'SORT';
+      }
+      type: string;
+    };
+    this.table.sortSelector = (_): TableSort<Experiment> => {
+      return {
+        sortBy: 'exp',
+        sortDirection: SortDirection.ASC,
+      };
+    };
+    this.table.propertyForActiveCheck = 'exp';
   }
 
   fetchExperiments(): void {
     this.isLoading = true;
     this.experimentsService.getExperiments().subscribe({
       next: (data) => {
-        this.experiments = data;
+        this.experiments = data as Experiment[];
+        if (this.table) {
+          this.table.rows = this.experiments;
+          this.table.init();
+        }
         this.isLoading = false;
       },
       error: (error) => {
@@ -49,7 +113,7 @@ export class ExperimentsComponent implements OnInit {
     this.fetchExperiments();
   }
 
-  shouldShowExperiment(index: number): boolean {
+  shouldMakeBoldExpName(index: number): boolean {
     if (index === 0) {
       return true;
     }
--- src/app/experiments/experiments.module.ts
+++ src/app/experiments/experiments.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatTableModule } from '@angular/material/table';
+import { CopyComponent } from '@app/shared/components/copy/copy.component';
+import { SharedModule } from '@shared/shared.module';
 import { ExperimentsRoutingModule } from './experiments-routing.module';
 import { ExperimentsComponent } from './experiments.component';
 
@@ -9,9 +10,10 @@ import { ExperimentsComponent } from './experiments.component';
   declarations: [ExperimentsComponent],
   imports: [
     CommonModule,
+    SharedModule,
+    CopyComponent,
     ExperimentsRoutingModule,
     MatProgressSpinnerModule,
-    MatTableModule,
   ],
 })
 export class ExperimentsModule {}
